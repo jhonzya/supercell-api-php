8c042e00aa044034400f65a6f4d60baed255fcdd
Jonathan Salinas <a.jonathan.sl@gmail.com>
2021-01-25 20:45:22 -0600

    skeleton

---
+++ src/BrawlStars.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jhonzya\SupercellApi;
+
+class BrawlStars extends Supercell
+{
+    protected $api = 'https://api.brawlstars.com/v1/';
+}
---
+++ tests/BrawlStarsBase.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jhonzya\SupercellApi\Test;
+
+use Jhonzya\SupercellApi\BrawlStars;
+
+class BrawlStarsBase extends SupercellBase
+{
+    protected $classname = BrawlStars::class;
+
+    /**
+     * @param string $file
+     * @return false|string
+     */
+    protected function getJsonResponse(string $file)
+    {
+        return file_get_contents(__DIR__. "/Mocks/BrawlStars/{$file}");
+    }
+}
